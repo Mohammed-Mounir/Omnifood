b5c80ccada2d53556ef1a24cd7e5c4f09d39a35f
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-09-29 14:53:51 +0200

    Implemented smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,32 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
+
+// Smooth scroll
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to sections
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile nav menu
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.remove("nav-open");
+    }
+  });
+});
